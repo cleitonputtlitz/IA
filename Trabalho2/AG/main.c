fc7993ab20b23fe7d059ee4a2ef11fe352a5ba97
Patrick <patrick.bastiani@hotmail.com>
2014-10-01 20:28:34 -0300

    Criação e Leitura de arquivo.

---
+++ Trabalho2/AG/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+#define TM 9
+
+void criaArquivoEntrada(){
+	FILE *f = fopen("entrada.txt", "w+");
+	
+	if(f == NULL){
+		return;
+	}else{
+		int i, j, numero;
+		for(i=0; i < TM; i++){
+			for(j=0; j < TM; j++){
+				if(i == j){
+					fprintf(f, "%d;", 0);
+				}else{
+					numero = rand()%20;
+					if(numero <= 1){
+						numero = -1;
+					}else{
+						numero = rand()%20;
+						numero += 1;
+					}
+					fprintf(f, "%d;", numero);
+				}
+			}
+			fprintf(f, "\n");
+		}
+		fclose(f);
+	}
+}
+
+int contaTamanhoMatriz(FILE *fp){
+	int nElementos = 1;
+	int valor = 0;
+	rewind(fp);
+	if(fp != NULL){
+		while(!feof(fp)){
+			nElementos += fscanf(fp, "%d;", &valor);
+		}
+	}
+	return (int)sqrt(nElementos);
+}
+
+int main(){
+	srand(time(NULL));
+	criaArquivoEntrada();
+	FILE *arq = fopen("entrada.txt", "r");
+	int nElementos = 1;
+	int valor = 0;
+	int X = contaTamanhoMatriz(arq);
+	int tabela[X][X];
+	int i=0, j=0;
+	
+	rewind(arq);
+	if(arq != NULL){
+		i = 0;
+		j = 0;
+		while(!feof(arq)){
+			nElementos += fscanf(arq, "%d;", &valor);
+			tabela[i][j] = valor;
+			j++;
+			if(i >= X) break;
+			if(j >= X){
+				j = 0;
+				i++;
+			}
+		}
+		fclose(arq);
+	}
+	
+	for(i=0; i < X; i++){
+		for(j=0; j < X; j++){
+			printf("%d;", tabela[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
